52a07a3ce6afcfe343523a45646c273c3bddffb2
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-04 14:37:46 -0800

    Refresher on how to call methods

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+MethodPractice();
+
 Console.WriteLine("Hello, GitHub!");
+
+for (int i = 1; i <= 10; i++)
+{ //for loop refresher 
+    Console.WriteLine($"Number: {i}");
+}
+
+static void MethodPractice()
+{
+    Console.WriteLine("I'm an example of how calling methods work!!");
+}
+
